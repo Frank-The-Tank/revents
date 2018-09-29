2cce215720f6db60b7614c3f7f878cdb5aa4563f
Frank Garcia <futboler18@yahoo.com>
2018-09-29 09:14:27 -0700

    added redux thunk to the project to facilitate async fetching from firestore in the future

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,15 +2,17 @@ import { combineReducers } from "redux";
 import { reducer as FormReducer } from "redux-form";
 import testReducer from "../../features/testarea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
-import modalsReducer from "../../features/modals/modalReducer"
-import authReducer from "../../features/auth/authReducer"
+import modalsReducer from "../../features/modals/modalReducer";
+import authReducer from "../../features/auth/authReducer";
+import asyncReducer from "../../features/async/asyncReducer";
 
 const rootReducer = combineReducers({
   form: FormReducer,
   test: testReducer,
   events: eventReducer,
   modals: modalsReducer,
-  auth: authReducer
+  auth: authReducer,
+  async: asyncReducer
 });
 
 export default rootReducer;
